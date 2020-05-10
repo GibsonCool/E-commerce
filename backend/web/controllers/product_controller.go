efc950f7936bf816f7b96e94bb53629491b2eaa8
DoubleX <double@tg10010.com>
2020-05-10 16:07:57 +0700

    add： 商品管理接口完成

--- backend/web/controllers/product_controller.go
+++ backend/web/controllers/product_controller.go
@@ -2,18 +2,23 @@ package controllers
 
 import (
 	"E-commerce/common/conf"
+	"E-commerce/datamodels"
 	"E-commerce/services"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/mvc"
+	"github.com/unknwon/com"
 )
 
 type ProductController struct {
-	Ctx          iris.Context
-	OrderService services.IProductService
+	Ctx            iris.Context
+	ProductService services.IProductService
 }
 
-func (pc *ProductController) GetAll() mvc.View {
-	product, err := pc.OrderService.GetAllProduct()
+// 获取商品列表
+// url:  /product/all
+// type: get
+func (p *ProductController) GetAll() mvc.View {
+	product, err := p.ProductService.GetAllProduct()
 	if err != nil {
 		conf.AppSetting.Logger.Error(err.Error())
 	}
@@ -24,4 +29,86 @@ func (pc *ProductController) GetAll() mvc.View {
 			"productArray": product,
 		},
 	}
+}
+
+//修改商品
+// url:  /product/update
+// type: post
+func (p *ProductController) PostUpdate() {
+	product := &datamodels.Product{
+		ProductName:  p.Ctx.FormValue("ProductName"),
+		ProductNum:   com.StrTo(p.Ctx.FormValue("ProductNum")).MustInt64(),
+		ProductImage: p.Ctx.FormValue("ProductImage"),
+		ProductUrl:   p.Ctx.FormValue("ProductUrl"),
+	}
+	product.ID = uint(com.StrTo(p.Ctx.FormValue("ID")).MustInt())
+	p.Ctx.Application().Logger().Error(product)
+
+	err := p.ProductService.UpdateProduct(product)
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+	p.Ctx.Redirect("/product/all")
+}
+
+// 添加商品页面
+// url:  /product/add
+// type: get
+func (p *ProductController) GetAdd() mvc.View {
+	return mvc.View{
+		Name: "product/add.html",
+	}
+}
+
+// 添加商品
+// url:  /product/all
+// type: post
+func (p *ProductController) PostAdd() {
+	product := &datamodels.Product{
+		ProductName:  p.Ctx.FormValue("ProductName"),
+		ProductNum:   com.StrTo(p.Ctx.FormValue("ProductNum")).MustInt64(),
+		ProductImage: p.Ctx.FormValue("ProductImage"),
+		ProductUrl:   p.Ctx.FormValue("ProductUrl"),
+	}
+	rows, err := p.ProductService.InsertProduct(product)
+	p.Ctx.Application().Logger().Error("插入数据：" + com.ToStr(rows))
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+	p.Ctx.Redirect("/product/all")
+}
+
+// 商品修改页面
+// url:  /product/manager
+// type: get
+func (p *ProductController) GetManager() mvc.View {
+	idString := p.Ctx.URLParam("id")
+	id := com.StrTo(idString).MustInt64()
+
+	product, err := p.ProductService.GetProductByID(id)
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+
+	return mvc.View{
+		Name: "product/manager.html",
+		Data: iris.Map{
+			"product": product,
+		},
+	}
+}
+
+// 删除商品
+// url:  /product/delete
+// type: get
+func (p *ProductController) GetDelete() {
+	idString := p.Ctx.URLParam("id")
+	id := com.StrTo(idString).MustInt64()
+	isOk := p.ProductService.DeleteProductByID(id)
+	if isOk {
+		p.Ctx.Application().Logger().Debug("删除商品成功，ID为：" + idString)
+	} else {
+		p.Ctx.Application().Logger().Debug("删除商品失败，ID为：" + idString)
+	}
+	p.Ctx.Redirect("/product/all")
 }
--- repositories/product_repository.go
+++ repositories/product_repository.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"E-commerce/datamodels"
+	"errors"
 	"github.com/jinzhu/gorm"
 )
 
@@ -22,24 +23,35 @@ func NewProduct(sqlDb *gorm.DB) *product {
 }
 
 func (p *product) Insert(d *datamodels.Product) (int64, error) {
-	panic("implement me")
+	result := p.sqlDb.Create(d)
+	return result.RowsAffected, result.Error
 }
 
 func (p *product) Delete(i int64) bool {
-	panic("implement me")
+	var product datamodels.Product
+	product.ID = uint(i)
+	err := p.sqlDb.Delete(&product)
+	return err != nil
 }
 
 func (p *product) Update(d *datamodels.Product) error {
-	panic("implement me")
+	return p.sqlDb.Save(d).Error
 }
 
 func (p *product) SelectByKey(i int64) (*datamodels.Product, error) {
-	panic("implement me")
+	var product datamodels.Product
+	// 先查询
+	e := p.sqlDb.First(&product, i).Error
+	if e != nil {
+		e = errors.New("商品查询不到：" + e.Error())
+	}
+
+	return &product, e
 }
 
 func (p *product) SelectAll() ([]*datamodels.Product, error) {
 	var products []*datamodels.Product
 	e := p.sqlDb.Find(&products).Error
 
-	return products,e
+	return products, e
 }
