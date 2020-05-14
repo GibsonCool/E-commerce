d3d062efeeb273f0e8078ee83b2a9d5a370941bd
DoubleX <double@tg10010.com>
2020-05-14 11:02:11 +0700

    add:服务端cookie 身份验证

--- validate.go
+++ validate.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"E-commerce/common"
+	"E-commerce/common/conf"
+	"E-commerce/encrypt"
 	"errors"
 	"fmt"
 	"net/http"
@@ -12,22 +14,49 @@ func Auth(w http.ResponseWriter, r *http.Request) error {
 	fmt.Println("执行验证！")
 
 	//添加基于cookie的权限验证
-	//err := CheckUserInfo(r)
-	//if err != nil {
-	//	return err
-	//}
+	err := CheckUserInfo(r)
+	if err != nil {
+		return err
+	}
 
-	return errors.New("自定义错误")
-	//return nil
+	//return errors.New("自定义错误")
+	return nil
+}
+
+func CheckUserInfo(r *http.Request) error {
+	// 获取 uid , cookie
+	uidCookie, err := r.Cookie("uid")
+	if err != nil {
+		return errors.New("用户 UID cookie 获取失败：" + err.Error())
+	}
+
+	// 获取 UID 加密串
+	signCookie, err := r.Cookie("sign")
+	if err != nil {
+		return errors.New("用户 sign cookie 获取失败：" + err.Error())
+	}
+
+	// 解密
+	signByte, err := encrypt.DePwdCode(signCookie.Value)
+	if err != nil {
+		return errors.New("用户 sign cookie 解密失败，被篡改：" + err.Error())
+	}
+	fmt.Println("用户ID:" + uidCookie.Value + "  解密后ID：" + string(signByte))
+	if uidCookie.Value == string(signByte) {
+		return nil
+	}
+	return errors.New("身份校验失败，" + "用户ID:" + uidCookie.Value + "  解密后ID：" + string(signByte))
 }
 
 //执行正常业务逻辑
 func Check(w http.ResponseWriter, r *http.Request) {
 	//执行正常业务逻辑
-	fmt.Println("执行check！")
+	w.Write([]byte("检验通过，执行正常业务逻辑"))
 }
 
 func main() {
+
+	conf.Setup()
 
 	// 1.创建过滤器
 	filter := common.NewFilter()
